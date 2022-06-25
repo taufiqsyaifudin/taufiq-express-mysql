e34cd6e85e794b4894cca15e649dee5b30ac5bb7
taufiqsyaifudin <taufiqsyaifudin.dev@gmail.com>
2022-06-25 21:17:48 +0700

    API express dan mysql

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   })
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`app running in ${process.env.NODE_ENV} on ${PORT}`)
